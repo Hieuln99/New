8f802674851bc1efb9d16ff0ff47be054ca7b9cc
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2021-08-15 17:00:13 +0700

    ok

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const {Int32} = require('mongodb');
 const exphbs = require('express-handlebars');
-const { DeleteS, Search, updateStudent, getAll, getPro, insert} = require('./databasehandle');
+const { Search, getAll, getPro, insertP, DeleteP, updatePro} = require('./databasehandle');
 
 const app = express()
 app.engine('handlebars',exphbs())
@@ -15,41 +15,27 @@ app.post('/insert', async (req,res)=>{
     const picInput= req.body.txtPic;
     const priceInput= req.body.txtPrice;
     const newPro = {name: nameInput,pic: picInput,price: Int32(priceInput)};
-    await insert(newPro);
-// chuyen huong den file index
+    await insertP(newPro);
     res.redirect('/');
-})
-app.post('/insert1', async (req,res)=>{
-    const nameInput = req.body.txtName;
-    const tuoiInput= req.body.txtTuoi;
-    const newStudent = {name: nameInput,tuoi: Int32(tuoiInput)};
-    await insertStudent(newStudent);
-// chuyen huong den file index
-    res.render('addToy');
 })
 
 app.get('/delete',async (req,res)=>{
     const IdInput = req.query.id;
-    await DeleteS(IdInput)
+    await DeleteP(IdInput)
     res.redirect('/');
-})
-app.get('/addToy',(_req,res)=>{
-    res.render('addToy')
-})
-app.get('/information',(_req,res)=>{
-    res.render('information')
 })
 
 app.post('/search',async (req,res)=>{
     const SearchIn = req.body.txtSearch;
-    const allStudents = await Search(SearchIn);
-    res.render('index',{data:allStudents})
+    const all = await Search(SearchIn);
+    res.render('index',{data:all})
 })
 app.post('/update',async (req,res)=>{
     const Id = req.body.id;
-    const inputName = req.body.txtName;
-    const inputTuoi= req.body.txtTuoi;
-    await updateStudent(Id,inputName,inputTuoi)
+    const nameInput = req.body.txtName;
+    const picInput= req.body.txtPic;
+    const priceInput= req.body.txtPrice;
+    await updatePro(Id,nameInput,picInput,priceInput)
     res.redirect('/');
 })
 
@@ -59,9 +45,32 @@ app.get('/edit',async (req,res)=>{
     res.render('edit',{pro: Search});
 })
 
+app.get('/search',async (_req,res)=>{
+    const all =  await getAll();
+    res.render('edit',{data:all})
+})
+
+// chuyen trang
+app.get('/addToy',(_req,res)=>{
+    res.render('addToy')
+})
+app.get('/information',(_req,res)=>{
+    res.render('information')
+})
+
 app.get('/',async (_req,res)=>{
     const all =  await getAll();
     res.render('index',{data:all})
+})
+
+app.get('/editfirst',async (req,res)=>{
+    const all =  await getAll();
+    res.render('edit',{data:all})
+})
+
+app.get('/about',async (req,res)=>{
+    const all =  await getAll();
+    res.render('information',{data:all})
 })
 
 const PORT = process.env.PORT ||5000;
--- src/databasehandle.js
+++ src/databasehandle.js
@@ -8,12 +8,12 @@ async function getdb() {
     const dbo = client.db(DATABASE_NAME);
     return dbo;
 }
-async function insert(newStudent) {
+async function insertP(newProduct) {
     const dbo = await getdb();
-    const newS = await dbo.collection(DATABASE_NAME).insertOne(newStudent);
+    const newS = await dbo.collection(DATABASE_NAME).insertOne(newProduct);
     console.log("ID: ", newS.insertedId.toHexString());
 }
-async function DeleteS(IdInput) {
+async function DeleteP(IdInput) {
     const dbo = await getdb();
     await dbo.collection(DATABASE_NAME).deleteOne({ _id: ObjectId(IdInput) });
 }
@@ -23,17 +23,17 @@ async function getPro(Id) {
 }
 async function getAll() {
     const dbo = await getdb();
-    const allStudents = await dbo.collection(DATABASE_NAME).find({}).toArray();
-    return allStudents;
+    const all = await dbo.collection(DATABASE_NAME).find({}).toArray();
+    return all;
 }
-async function updateStudent(id,nameInput,tuoiInput){
+async function updatePro(id,nameInput,picInput,priceInput){
     const dbo  = await getdb();
-    dbo.collection(DATABASE_NAME).updateOne({_id:ObjectId(id)},{$set:{name: nameInput, tuoi: tuoiInput}})
+    dbo.collection(DATABASE_NAME).updateOne({_id:ObjectId(id)},{$set:{name: nameInput, pic: picInput, price: priceInput}})
 }
 async function Search(SearchIn) {
     const dbo = await getdb();
-    const allStudents = await dbo.collection(DATABASE_NAME).find({ name: SearchIn }).toArray();
-    return allStudents;
+    const all = await dbo.collection(DATABASE_NAME).find({ name: SearchIn }).toArray();
+    return all;
 }
 
-module.exports={getdb, insert, DeleteS, updateStudent,getPro, Search, getAll}
+module.exports={getdb, insertP, DeleteP, updatePro,getPro, Search, getAll}
